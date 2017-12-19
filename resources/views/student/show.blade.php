8c1ca9341591ded39ea5ad212ffda47deaa66174
Melvin San Jose <sanjose.melvin@gmail.com>
2017-12-20 05:55:51 +0800

    Add label for action buttons column

--- resources/views/student/show.blade.php
+++ resources/views/student/show.blade.php
@@ -1,108 +1,105 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container">
-		<script type="text/javascript">
-			document.title = '{{$student->name()}}' + ' - ' + document.title;
-		</script>
-		
+<div class="container">
+	<script type="text/javascript">
+		document.title = '{{$student->name()}}' + ' - ' + document.title;
+	</script>
 
-		@if(isset($student))
+
+	@if(isset($student))
+	<table class="table table-striped">
+		<tr><th>Name</th>
+			<td>{{$student->name()}}</td>
+			<td rowspan="5">
+				<img src="{{$student->picture()}}" style="max-height: 180px;">
+			</td>
+		</tr>
+		<tr><th>Student number</th><td>{{$student->student_number()}}</td></tr>
+		<tr><th>Program</th>
+			<td>
+				<a href="/program/{{$student->program->short_name}}">
+					{{$student->program->name}}
+				</a></td>
+
+			</tr>
+			<tr>
+				<th>Department</th>
+				<td><a href="/department/{{$student->department()->short_name}}">
+					{{$student->department()->name}}
+				</a></td>
+			</tr>
+			<tr>
+				<th>College</th>			
+				<td><a href="/college/{{$student->college()->short_name}}">
+					{{$student->college()->name}}
+				</a></td>
+
+			</tr>
+
+		</table>
+
+
+		<h4 class="page-header">Deficiencies </h4>
+
+		<div id="deficiency-table">
 			<table class="table table-striped">
-				<tr><th>Name</th>
-					<td>{{$student->name()}}</td>
-					<td rowspan="5">
-						<img src="{{$student->picture()}}" style="max-height: 180px;">
-					</td>
-				</tr>
-				<tr><th>Student number</th><td>{{$student->student_number()}}</td></tr>
-				<tr><th>Program</th>
-					<td>
-						<a href="/program/{{$student->program->short_name}}">
-						{{$student->program->name}}
-					</a></td>
 
-				</tr>
 				<tr>
 					<th>Department</th>
-					<td><a href="/department/{{$student->department()->short_name}}">
-						{{$student->department()->name}}
-					</a></td>
+					<th>Title</th>
+					<th>Note</th>
+					<th>Posted By</th>
+					<th>Posted On</th>
+					<th>Actions</th>
 				</tr>
+
+
+				@foreach($student->deficiencies as $deficiency)
+
 				<tr>
-					<th>College</th>			
-						<td><a href="/college/{{$student->college()->short_name}}">
-								{{$student->college()->name}}
-						</a></td>
-					
+					<td><a href="/department/{{$deficiency->department->short_name}}">
+						<span class="visible-xs" title="{{$deficiency->department->name}}">
+							{{strtoupper($deficiency->department->short_name)}}
+						</span>
+						<span class="hidden-xs">{{$deficiency->department->name}}</span>
+					</a></td>
+
+					<td>{{$deficiency->title}}</td>
+
+					<td>{{$deficiency->note}}</td>
+					<td><a href="{{$deficiency->staff->linkTo()}}">
+						{{$deficiency->postedBy()}}
+					</a></td>
+
+
+					<td title="{{ $deficiency->postDateTime() }}">
+						{{ $deficiency->postDate() }}
+					</td>
+
+					<td>
+						@userInSameDepartment($deficiency->department)
+						<a href="#"><span class="glyphicon glyphicon-edit" title="Edit"></span></a>
+						&nbsp;
+						<a href="#"><span class="glyphicon glyphicon-remove" title="Remove"></span></a>
+						@enduserInSameDepartment
+					</td>
 				</tr>
+
+				@endforeach
 
 			</table>
+		</div>
 
 
-			<h4 class="page-header">Deficiencies </h4>
-			
-			<div id="deficiency-table">
-				<table class="table table-striped">
-			
-								<tr>
-									<th>Department</th>
-									<th>Title</th>
-									<th>Note</th>
-									<th>Posted By</th>
-									<th>Posted On</th>
-									<th>&nbsp;</th>
-								</tr>
-								
-								
-									@foreach($student->deficiencies as $deficiency)
-			
-									<tr>
-										<td><a href="/department/{{$deficiency->department->short_name}}">
-											<span class="visible-xs" title="{{$deficiency->department->name}}">
-												{{strtoupper($deficiency->department->short_name)}}
-											</span>
-											<span class="hidden-xs">{{$deficiency->department->name}}</span>
-										</a></td>
-										
-										<td>{{$deficiency->title}}</td>
-									
-										<td>{{$deficiency->note}}</td>
-										<td><a href="{{$deficiency->staff->linkTo()}}">
-											{{$deficiency->postedBy()}}
-										</a></td>
-			
-			
-										<td title="{{ $deficiency->postDateTime() }}">
-											{{ $deficiency->postDate() }}
-										</td>
-			
-										<td>
-											@userInSameDepartment($deficiency->department)
-												<a href="#"><span class="glyphicon glyphicon-edit" title="Edit"></span></a>
-												&nbsp;
-												<a href="#"><span class="glyphicon glyphicon-remove" title="Remove"></span></a>
-											@enduserInSameDepartment
-										</td>
-									</tr>
-								
-								@endforeach
-			
-							</table>
-						</div>
-				
+		@hasRole('staff')
+		<a title="File deficiency" href="#" class="btn btn-danger btn-sm pull-right">ADD NEW <span class="glyphicon glyphicon-plus"></span></a>
 
-				@hasRole('staff')
-					<a title="File deficiency" href="#" class="btn btn-danger btn-sm pull-right">ADD NEW <span class="glyphicon glyphicon-plus"></span></a>
-				
-				@endhasRole
-						
+		@endhasRole
 
-		@else
-		Not found bruh
 
 		@endif
-    </div>
+	</div>
 
-    
-@endsection
+
+	@endsection
