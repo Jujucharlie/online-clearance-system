c373466204afeb713643894489b80ecd148c44d5
Melvin San Jose <sanjose.melvin@gmail.com>
2018-01-03 01:46:22 +0800

    Error checking for marking completed deficiencies

--- app/Http/Controllers/DeficiencyController.php
+++ app/Http/Controllers/DeficiencyController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Deficiency;
+use App\Staff;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\URL;
 
 class DeficiencyController extends Controller
@@ -12,9 +14,25 @@ class DeficiencyController extends Controller
 	//Triggered when authorized user clicks "delete" button
 	public function destroy($id)
 	{
+		$user = Auth::user();
+
+		if(!$user){
+			abort(403);
+		}
+
+		$staff = Staff::whereUserId($user->id)->firstOrFail();
+
 		$def = Deficiency::findOrFail($id);
+
+		if($staff->department != $def->department){
+			$flash_message = "Sorry, you are not allowed to do that.";
+			flash($flash_message)->error()->important();
+
+			return redirect()->back();
+		}
 		//Display confirmation dialog (modal, maybe?)
 		
+
 		$flash_message = "Deficiency <strong>" . $def->title . "</strong> marked as completed.";
 		flash($flash_message)->success();
 
