a94959c550302d13115126852a7993b72d6ea4c7
Melvin San Jose <sanjose.melvin@gmail.com>
2018-01-09 14:35:12 +0800

    Add more fields to edit form

--- resources/views/student/deficiencyeditmodal.blade.php
+++ resources/views/student/deficiencyeditmodal.blade.php
@@ -16,11 +16,35 @@
 				{{ Form::token() }}
 			<div class="modal-body">
 				<div class="form-group">
+					{{ Form::label('department_id', 'Department') }}
+					{{ Form::text('department_id',Department::find($deficiency
+						->department_id)->name,
+						array("class" => "form-control", "readonly")
+					) }}
+				</div>
+				
+				<div class="form-group">
+					{{ Form::label('staff_id', 'Staff') }}
+					{{ Form::text('staff_id',
+						Staff::find($deficiency->staff_id)->name(),
+						array("class" => "form-control", "readonly")
+					) }}
+				</div>
+				
+				<div class="form-group">
 					{{ Form::label('title', 'Title') }}
 					{{ Form::text('title', $deficiency->title,
 						array("class" => "form-control")
 					) }}
 				</div>
+
+				<div class="form-group">
+					{{ Form::label('completed', 'Completed') }}
+					{{ Form::checkbox('completed', $deficiency->completed,
+						array("class" => "form-control")
+					) }}
+				</div>
+				
 				<div class="form-group">
 					{{ Form::label('title', 'Note') }}
 					{{ Form::textarea('note', $deficiency->note,
